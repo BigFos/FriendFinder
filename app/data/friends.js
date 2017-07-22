f39c9af7a8ece78395432f0e955e380267d46658
Steven Foster Jr <Steven Foster Jr>
2017-07-21 23:16:18 -0700

    server and routes working

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendArray =[
+{
+  "name":"Tommy",
+  "photo":"https://33hpwq10j9luq8gl43e62q4e-wpengine.netdna-ssl.com/wp-content/uploads/2017/04/03IWPVYyhyq2xgY3X.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}
+];
+
+module.exports = friendArray;
